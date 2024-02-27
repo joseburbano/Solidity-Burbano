eaab7beec54765d939dbd28e90ad08adad1943bd
jose manuel burbano <jose.burbano@getpulpo.com>
2024-02-27 18:38:45 -0500

    Solidity

--- BLOCK 1/Topic 1 -  first steps/basic_concepts.sol
+++ BLOCK 1/Topic 1 -  first steps/basic_concepts.sol
@@ -5,13 +5,13 @@ pragma solidity >=0.4.4 <0.7.0;
 import "./ERC20.sol";
 
 contract FirstContract {
-//In this variable is the address of the person deploying the contract.
+    //In this variable is the address of the person deploying the contract.
     address owner;
     ERC20Basic token;
-/*
-We store in the owner variable the address of the person who displays the contract.
-initialize the number of tokens
-*/
+    /*
+    We store in the owner variable the address of the person who displays the contract.
+    initialize the number of tokens
+    */
     constructor() public {
         owner = msg.sender;
         token = new ERC20Basic(1000);
