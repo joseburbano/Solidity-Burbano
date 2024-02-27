981fbf38012e0cfea1b3828b9a560deaea90f7ca
jose manuel burbano <jose.burbano@getpulpo.com>
2024-02-27 18:53:01 -0500

    Solidity

---
+++ BLOCK 1/Topic 2 - Transaction and block properties/global-functions.sol
@@ -0,0 +1,41 @@
+//Started choose the version
+pragma solidity >>=0.4.4 <0.7.0;
+
+contract globalFunctions {
+
+    //Function msg.sender
+    function MsgSender() public view returns (address){
+        return msg.sender;
+    }
+
+     //Function now
+    function Now() public view returns (address){
+        return now;
+    }
+
+    //Function block.coinbase
+    function BlockCoinbase() public view returns (address){
+        return block.coinbase;
+    }
+
+    //Function block.difficulty
+    function BlockDifficulty() public view returns (uint){
+        return block.difficulty;
+    }
+
+
+    //Function block.number
+    function BlockNumber() public view returns (uint){
+        return block.number;
+    }
+
+    //Function msg.sif
+    function MsgSif() public view returns (bytes4){
+        return msg.sig;
+    }
+
+    //Function tx.gasprice
+    function TxGasPrice() public view returns (uint){
+        return tx.gasprice;
+    }
+}
