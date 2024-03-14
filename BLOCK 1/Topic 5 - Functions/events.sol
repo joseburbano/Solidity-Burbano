baf35abb2b4a08cf7aea58c2c321bbe8c949757c
jose manuel burbano <jose.burbano@getpulpo.com>
2024-03-14 18:54:38 -0500

    Solidity

--- BLOCK 1/Topic 5 - Functions/events.sol
+++ BLOCK 1/Topic 5 - Functions/events.sol
@@ -1,29 +1,30 @@
 // SPDX-License-Identifier: MIT
+//Indicates the version
 pragma solidity >=0.4.4 <0.7.0;
 pragma experimental ABIEncoderV2;
 
-contract Eventos{
+contract Events{
 
-    //Declararmos los eventos a utilizar
-    event nombre_evento1 (string _nombrePersona);
-    event nombre_evento2 (string _nombrePersona, uint _edadPersona);
-    event nombre_evento3(string, uint, address, bytes32);
-    event abortarmision();
+    //Declare the events to be used
+    event event_name1 (string _namePerson);
+    event event_name2 (string _namePerson, uint _agePerson);
+    event event_name3(string, uint, address, bytes32);
+    event abortmision();
 
-    function EmitirEvento1(string memory _nombrePersona) public{
-        emit nombre_evento1(_nombrePersona);
+    function IssueEvent1(string memory _namePerson) public{
+        emit event_name1(_namePerson);
     }
 
-    function EmitirEvento2(string memory _nombrePersona, uint _edad) public{
-        emit nombre_evento2(_nombrePersona, _edad);
+    function IssueEvent2(string memory _namePerson, uint _age) public{
+        emit event_name2(_namePerson, _age);
     }
 
-    function EmitirEvento3(string memory _nombrePersona, uint _edad) public{
-        bytes32 hash_id = keccak256(abi.encodePacked(_nombrePersona, _edad, msg.sender));
-        emit nombre_evento3(_nombrePersona, _edad, msg.sender, hash_id);
+    function EmitirEvento3(string memory _namePerson, uint _age) public{
+        bytes32 hash_id = keccak256(abi.encodePacked(_namePerson, _age, msg.sender));
+        emit event_name3(_namePerson, _age, msg.sender, hash_id);
     }
 
-    function AbortarMision() public {
-        emit abortarmision();
+    function AbortMission() public {
+        emit abortmision();
     }
 }
