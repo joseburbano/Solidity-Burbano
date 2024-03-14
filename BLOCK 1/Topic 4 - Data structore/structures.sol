f7fc76399f6f9a3703f04a02de6026b1474802bd
jose manuel burbano <jose.burbano@getpulpo.com>
2024-03-14 15:37:51 -0500

    Solidity

--- BLOCK 1/Topic 4 - Data structore/structures.sol
+++ BLOCK 1/Topic 4 - Data structore/structures.sol
@@ -1,11 +1,11 @@
 // SPDX-License-Identifier: MIT
-//Especificamos la version
+//Indicates the version
 pragma solidity >=0.4.4 <0.7.0;
 
-contract Estructuras{
-    
+contract Estructuras {
+
     //Cliente de una pagina web de pago
-    struct cliente{
+    struct cliente {
         uint id;
         string name;
         string dni;
@@ -14,30 +14,29 @@ contract Estructuras{
         uint credit_number;
         uint secret_number;
     }
-    
+
     //Declaramos una variable de tipo cliente
-    cliente cliente_1 = cliente(1,"Joan", "12345678B", "joan@udemy.com", 12345678, 1234, 11);
-    
+    cliente cliente_1 = cliente(1, "Joan", "12345678B", "joan@udemy.com", 12345678, 1234, 11);
+
     //Amazon (cualquier pagina de compra venta de productos)
-    struct producto{
+    struct producto {
         string nombre;
         uint precio;
     }
-    
+
     //Declaramos una variable de tipo producto
     producto movil = producto("samsung", 300);
-     
-    
+
     //Proyecto cooperativo de ONGs para ayudar en diversas causas
-    struct ONG{
+    struct ONG {
         address ong;
         string nombre;
     }
     //Declaramos una variable de tipo ONG
     //ONG caritas;
     ONG caritas = ONG(0x5B38Da6a701c568545dCfcB03FcB875f56beddC4, "Caritas");
-    
-    struct Causa{
+
+    struct Causa {
         uint id;
         string nombre;
         uint precio_objetivo;
