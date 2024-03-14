ef01d7a9c1c9eb7c214406358554cc0bba6bbd71
jose manuel burbano <jose.burbano@getpulpo.com>
2024-03-14 13:39:34 -0500

    Solidity

--- BLOCK 1/Topic 3 - Types of variables and operators/compare-variables.sol
+++ BLOCK 1/Topic 3 - Types of variables and operators/compare-variables.sol
@@ -1,18 +1,18 @@
 // SPDX-License-Identifier: MIT
-//Indicar la version
+//Indicates the version
 pragma solidity >=0.4.4 <0.7.0;
 pragma experimental ABIEncoderV2;
 
-contract compararStrings{
+contract compareStrings {
 
-    function Comparar(string memory _j ,string memory _i) public pure returns(bool){
+    function Compare(string memory _j, string memory _i) public pure returns (bool){
 
         bytes32 hash_i = keccak256(abi.encodePacked(_i));
         bytes32 hash_j = keccak256(abi.encodePacked(_j));
 
-        if(hash_i == hash_j){
+        if (hash_i == hash_j) {
             return true;
-        }else{
+        } else {
             return false;
         }
 
--- BLOCK 1/Topic 3 - Types of variables and operators/modifiers.sol
+++ BLOCK 1/Topic 3 - Types of variables and operators/modifiers.sol
@@ -1,31 +1,29 @@
 // SPDX-License-Identifier: MIT
-//Indicar la version
+//Indicates the version
 pragma solidity >=0.4.4 <0.7.0;
 pragma experimental ABIEncoderV2;
 
 contract public_private_internal{
 
-    //Modificador public
-    uint public mi_entero = 45;
-    string public mi_string = "Joan";
+    //Modifier public
+    uint public my_entero = 45;
+    string public my_string = "Joan";
     address public owner;
 
     constructor() public{
         owner = msg.sender;
     }
 
-    //Modificador private
-    uint private mi_entero_privado = 10;
+    //Modifier private
+    uint private my_private_whole = 10;
     bool private flag =true;
 
     function test(uint _k) public{
-        mi_entero_privado = _k;
+        my_private_whole = _k;
     }
 
-    //Modificador internal
-    bytes32 internal hash = keccak256(abi.encodePacked("hola"));
-    address internal direccion = 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4;
-
-
+    //Modifier internal
+    bytes32 internal hash = keccak256(abi.encodePacked("hello"));
+    address internal addresse = 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4;
 
 }
