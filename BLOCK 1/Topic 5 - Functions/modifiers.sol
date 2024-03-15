84d6c23a36d7c860ea037f463b222fc69d0d558f
jose manuel burbano <jose.burbano@getpulpo.com>
2024-03-15 14:11:42 -0500

    Solidity

--- BLOCK 1/Topic 5 - Functions/modifiers.sol
+++ BLOCK 1/Topic 5 - Functions/modifiers.sol
@@ -3,49 +3,48 @@
 pragma solidity >=0.4.4 <0.7.0;
 pragma experimental ABIEncoderV2;
 
-contract view_pure_payable{
+contract view_pure_payable {
 
-    //Modificador de view
-    string[] lista_alumnos;
+    //Modifier of view
+    string[] list_students;
 
-    function nuevo_alumno(string memory _alumno) public{
-        lista_alumnos.push(_alumno);
+    function new_student(string memory _student) public {
+        list_students.push(_student);
     }
 
-    function ver_alumno(uint _posicion) public view returns(string memory){
-        return lista_alumnos[_posicion];
+    function see_student(uint _position) public view returns (string memory){
+        return list_students[_position];
     }
 
-    uint x=10;
-    function sumarAx(uint _a) public view returns(uint){
-        return x+_a;
+    uint x = 10;
+
+    function addAx(uint _a) public view returns (uint){
+        return x + _a;
     }
 
-    //Modificador de pure
+    //Modifier of pure
 
-    function exponenciacion(uint _a, uint _b) public pure returns(uint){
-        return _a**_b;
+    function exponentiation(uint _a, uint _b) public pure returns (uint){
+        return _a ** _b;
     }
 
-    //Modificador de payable
+    //Modifier of payable
 
-    mapping(address=>cartera) DineroCartera;
+    mapping(address => portfolio) MoneyWallet;
 
-    struct cartera{
-        string nombre_persona;
-        address direccion_persona;
-        uint dinero_persona;
+    struct portfolio {
+        string name_person;
+        address address_person;
+        uint money_person;
     }
 
-    function Pagar(string memory _nombrePersona, uint _cantidad) public payable{
-        cartera memory mi_cartera;
-        mi_cartera = cartera(_nombrePersona, msg.sender, _cantidad);
-        DineroCartera[msg.sender] = mi_cartera;
+    function Pay(string memory _namePerson, uint _quantity) public payable {
+        portfolio memory my_portfolio;
+        my_wallet = portfolio(_namePerson, msg.sender, _quantity);
+        MoneyWallet[msg.sender] = mi_cartera;
     }
 
-    function verSaldo() public view returns(cartera){
-        return DineroCartera[msg.sender];
+    function seeBalance() public view returns (portfolio){
+        return MoneyWallet[msg.sender];
     }
-
-
 }
