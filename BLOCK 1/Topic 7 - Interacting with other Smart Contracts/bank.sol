90de0590cb92d4cb2adf867e50aee83f0e10b85e
jose manuel burbano <jose.burbano@getpulpo.com>
2024-03-16 09:42:45 -0500

    Solidity

--- BLOCK 1/Topic 7 - Interacting with other Smart Contracts/bank.sol
+++ BLOCK 1/Topic 7 - Interacting with other Smart Contracts/bank.sol
@@ -1,30 +1,30 @@
 // SPDX-License-Identifier: MIT
-//Indicamos la version
+//Indicates the version
 pragma solidity >=0.4.4 <0.7.0;
 
-contract Banco{
-    
+contract Bank {
+
     //Definimos un tipo de dato complejo cliente
-    struct cliente{
+    struct cliente {
         string _nombre;
         address direccion;
         uint dinero;
     }
-    
+
     //mapping que nos relacionar el nombre del cliente con el tipo de dato cliente
-    mapping (string => cliente) clientes;
-    
+    mapping(string => cliente) clientes;
+
     //Funcion que nos permita dar de alta un nuevo cliente
-    
+
     function nuevoCliente(string memory _nombre) public {
         clientes[_nombre] = cliente(_nombre, msg.sender, 0);
     }
 }
 
-contract Banco2{
-    
+contract Banco2 {
+
 }
 
-contract Banco3{
-    
+contract Banco3 {
+
 }
