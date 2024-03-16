d40988dfd5666637c680e406d555b96357145e68
jose manuel burbano <jose.burbano@getpulpo.com>
2024-03-16 15:22:39 -0500

    Solidity

--- BLOCK 1/Topic 10 - Smart Contracts Factory/factory.sol
+++ BLOCK 1/Topic 10 - Smart Contracts Factory/factory.sol
@@ -3,7 +3,6 @@ pragma solidity >=0.4.4 <0.9.0;
 pragma experimental ABIEncoderV2;
 
 contract SmartContract1 {
-
     // Factory information storage
     mapping(address => address) public MyStaffContract;
 
@@ -14,7 +13,6 @@ contract SmartContract1 {
 }
 
 contract SmartContract2 {
-
     address public owner;
     constructor (address _address) public {
         owner = _address;
