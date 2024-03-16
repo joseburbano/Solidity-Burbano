2c6686c2a85e40943db50b56c3e74c213f168eea
jose manuel burbano <jose.burbano@getpulpo.com>
2024-03-16 09:38:03 -0500

    Solidity

--- BLOCK 1/Topic 5 - Functions/events.sol
+++ BLOCK 1/Topic 5 - Functions/events.sol
@@ -3,28 +3,28 @@
 pragma solidity >=0.4.4 <0.7.0;
 pragma experimental ABIEncoderV2;
 
-contract Events{
+contract Events {
 
     //Declare the events to be used
     event event_name1 (string _namePerson);
     event event_name2 (string _namePerson, uint _agePerson);
     event event_name3(string, uint, address, bytes32);
-    event abortmision();
+    event abortMision();
 
-    function IssueEvent1(string memory _namePerson) public{
+    function IssueEvent1(string memory _namePerson) public {
         emit event_name1(_namePerson);
     }
 
-    function IssueEvent2(string memory _namePerson, uint _age) public{
+    function IssueEvent2(string memory _namePerson, uint _age) public {
         emit event_name2(_namePerson, _age);
     }
 
-    function IssueEvent3(string memory _namePerson, uint _age) public{
+    function IssueEvent3(string memory _namePerson, uint _age) public {
         bytes32 hash_id = keccak256(abi.encodePacked(_namePerson, _age, msg.sender));
         emit event_name3(_namePerson, _age, msg.sender, hash_id);
     }
 
     function AbortMission() public {
-        emit abortmision();
+        emit abortMision();
     }
 }
--- BLOCK 1/Topic 6 - Loop and conditionals/loop-for.sol
+++ BLOCK 1/Topic 6 - Loop and conditionals/loop-for.sol
@@ -1,55 +1,55 @@
 // SPDX-License-Identifier: MIT
-//Especificar la version
+//Indicates the version
 pragma solidity >=0.4.4 <0.7.0;
 
-contract bucle_for{
+contract loop_for {
 
-    //Suma de los 100 primeros numeros a partir del numero introducido
+    //Sum of the first 100 numbers starting from the number entered.
 
-    function suma(uint _numero) public pure returns(uint){
+    function sum(uint _number) public pure returns (uint){
 
-        uint Suma = 0;
+        uint Sum = 0;
 
-        for(uint i =_numero; i<(100+_numero); i++){
-            Suma = Suma +i;
+        for (uint i = _number; i < (100 + _number); i++) {
+            Sum = Sum + i;
         }
 
-        return Suma;
+        return Sum;
     }
 
-    //Esto es un array dinamico de direcciones
-    address [] direcciones;
+    //This is a dynamic array of addresses
+    address [] addresses;
 
-    //Añade una direccion al array
-    function asociar() public{
-        direcciones.push(msg.sender);
+    //Add an address to the array
+    function associate() public {
+        addresses.push(msg.sender);
     }
 
-    //Comprobar si la direccion esta en el array de direcciones
-    function comprobarAsociacion() public view returns(bool, address){
+    //Check if the address is in the address array
+    function checkAssociation() public view returns (bool, address){
 
-        for(uint i=0; i< direcciones.length; i++){
-            if(msg.sender==direcciones[i]){
-                return (true, direcciones[i]);
+        for (uint i = 0; i < addresses.length; i++) {
+            if (msg.sender == addresses[i]) {
+                return (true, addresses[i]);
             }
         }
     }
 
-    //Doble for: Suma los 10 primeros factoriales
+    //Double for: Add the first 10 factorials.
     //n! = n*(n-1)*(n-2)*...*2*1
 
-    function sumaFactorial() public pure returns(uint){
+    function sumFactorial() public pure returns (uint){
 
-        uint suma=0;
-        for(uint i =1; i<=10; i++){
+        uint sum = 0;
+        for (uint i = 1; i <= 10; i++) {
 
             uint factorial = 1;
 
-            for(uint j=2; j<=i; j++){
-                factorial = factorial*j;
+            for (uint j = 2; j <= i; j++) {
+                factorial = factorial * j;
             }
 
-            suma = suma + factorial;
+            sum = sum + factorial;
         }
         return suma;
     }
